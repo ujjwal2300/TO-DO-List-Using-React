529fb22737a297a007f5fcfe0f1f7ca7bb0b6f88
Ujjwal Kumar <122315048+ujjwal2300@users.noreply.github.com>
2023-09-05 02:53:01 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,87 @@
+"use client"
+import React, { useState } from 'react'
+
+const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setMainTask] = useState([])
+
+  const submitHandler = (e)=>{
+    e.preventDefault()  // Here preventDefault block the form to get submitted
+    // console.log(title)
+    // console.log(desc)
+    setMainTask([...mainTask, {title, desc}])  // main Task me hmesa data add on krte ja rhe h
+    settitle("")
+    setdesc("")
+    console.log(mainTask)
+  }
+
+
+  const deleteHandler = (i) => {
+    let copyTask = [...mainTask]
+    copyTask.splice(i,1)
+    setMainTask(copyTask)
+  }
+
+  let renderTask = <h2>No Task Available</h2>;
+
+  if(mainTask.length > 0){
+    renderTask = mainTask.map((t, i) =>{
+      return (
+        <li key= {i} className='flex items-center justify-between mb-8'>
+        <div className='flex  items-center justify-between w-2/3'>
+          <h5 className='text-2xl font-semibold'>{t.title}</h5>
+          <h6 className='text-lg font-medium'>{t.desc}</h6>
+        </div>
+        <button
+        onClick={() => {
+          deleteHandler(i)
+        }}
+        className='bg-red-400 text-white px-4 py-2 rounded font-bold'>
+        Delete</button>
+        </li>
+      );
+    })
+  }
+  return (
+    <>
+    <h1 className='bg-black
+    text-white p-5 text-5xl font-bold text-center'>Ujjwal's ToDo- List</h1>
+    <form onSubmit={submitHandler}>
+    <input 
+      type="text" 
+      className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2'
+      placeholder='Enter Title Here'
+      value = {title}
+      // Two-way Binding Method. Here React and User both will see  the changes which we are doing on Title
+      onChange={(e) =>{
+      settitle(e.target.value)
+      }}
+    />
+
+      <input type="text" className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2'
+      placeholder='Enter Description Here' 
+      value = {desc}
+      onChange={(e) =>{
+        setdesc(e.target.value)
+        }}
+      />
+
+    <button className = "bg-black text-white px-4 py-3 text-2xl font-bold rounded">Add Task</button>
+
+
+    </form>
+    <hr />
+    <div className='p-8 bg-slate-200'>
+      <ul>
+        {renderTask}
+      </ul>
+
+    </div>
+
+
+    </>
+  )
+}
+
+export default page
